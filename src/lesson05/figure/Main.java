ac181a1ff979b490159ab51ed9bdf27972df3583
DiukarevSergii <vmesteprotiv@gmail.com>
2015-07-20 13:33:58 +0300

    sort figure

--- src/lesson05/figure/Main.java
+++ src/lesson05/figure/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Figure[] figureList = {
                 new Rectangle(10, 20), new Rectangle(1, 22),
                 new Circle(23), new Circle(10),
-                new Triangle(5, 5), new Triangle(10, 1)
+                new Triangle(5, 5, 4), new Triangle(4, 4, 2)
         };
         Arrays.sort(figureList);
         for (Figure f : figureList)
--- src/lesson05/figure/Triangle.java
+++ src/lesson05/figure/Triangle.java
@@ -2,17 +2,20 @@ package lesson05.figure;
 
 public class Triangle extends Figure {
 
-    protected double height;
-    protected double width;
+    protected double a;
+    protected double b;
+    protected double c;
 
-    public Triangle(int height, int width) {
-        this.height = height;
-        this.width = width;
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
         this.squar = this.getSquar();
     }
 
     @Override
     public double getSquar() {
-        return squar = 0.5 * this.height * this.width;
+        double p = (a + b + c) / 2;
+        return squar = Math.sqrt((p * (p - a) * (p - b) * (p - c)));
     }
 }
