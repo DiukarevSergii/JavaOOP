4b639762861d7d5eddec54dc7b5d88c223fd235e
DiukarevSergii <vmesteprotiv@gmail.com>
2015-07-24 13:38:40 +0300

    Constants

---
+++ src/util/Constants.java
@@ -0,0 +1,12 @@
+package util;
+
+/**
+ * @author Alexander Tyshchenko.
+ */
+public interface Constants {
+
+    String FILE_PATH_LESSON05_FINDFILES = "JavaOOP/resources";
+    String FILE_PATH_1234  = "JavaOOP/resources/1234.txt";
+    String FILE_PATH_HELLO  = "JavaOOP/resources/HELLO.txt";
+
+}
