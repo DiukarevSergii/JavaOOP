2cf62da6b30ca04a97552743ee8336ff7d0b533b
DiukarevSergii <vmesteprotiv@gmail.com>
2015-07-24 13:38:25 +0300

    Replacement characters inside one file

---
+++ src/lesson05/replacement/Replacement.java
@@ -0,0 +1,45 @@
+package lesson05.replacement;
+
+import util.Constants;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Replacement {
+    private static final String filePath = Constants.FILE_PATH_LESSON05_FINDFILES;
+
+    public static void main(String[] args) {
+        String fromFile = filePath + "/HELLO.txt";
+        String source = "";
+
+        try (FileReader reader = new FileReader(fromFile)) {
+            int c;
+
+            // Read and display the file.
+            while ((c = reader.read()) != -1) {
+                source += String.valueOf((char) c);
+            }
+            System.out.println(source);
+
+            source = source.replace("Hello", "1234");
+
+            System.out.println(source);
+
+            char buffer[] = new char[source.length()];
+            source.getChars(0, source.length(), buffer, 0);
+
+            try (FileWriter fw1 = new FileWriter(fromFile)) {
+                for (int i = 0; i < buffer.length; i++) {
+                    fw1.write(buffer[i]);
+                }
+
+            } catch (IOException e) {
+                System.out.println("An I/O Error Occured");
+            }
+
+        } catch (IOException e) {
+            System.out.println("I/O Error: " + e);
+        }
+    }
+}
