3913ceb138867555d85e9c6dfa38ceb3c189c200
DiukarevSergii <vmesteprotiv@gmail.com>
2015-07-24 18:03:40 +0300

    Constants

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -6,6 +6,7 @@ package util;
 public interface Constants {
 
     String FILE_PATH_LESSON05_FINDFILES = "JavaOOP/resources";
+    String FILE_PATH_LESSON05_INNER = "JavaOOP/resources/innerdir";
     String FILE_PATH_1234  = "JavaOOP/resources/1234.txt";
     String FILE_PATH_HELLO  = "JavaOOP/resources/HELLO.txt";
 
