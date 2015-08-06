c1561edceeed8be21c374d5130ff3d06b72b7289
Diukarev Sergii <vmesteprotiv@gmail.com>
2015-08-06 14:15:18 +0300

    Implement

--- src/lesson08/AnyTypeData.java
+++ src/lesson08/AnyTypeData.java
@@ -1,26 +1,40 @@
 package lesson08;
 
-/*Ќаписать класс, который умеет хранить в себе массив
-любых типов данных (int, long etc.). –еализовать метод,
-который возвращает любой элемент массива по индексу.*/
+//Write class, which can keep inside any type of data
+//Implement, method that return any element from array by index
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class AnyTypeData {
 
     private static class AnyType{
+        protected List<Integer> listInt = new ArrayList<>(20);
+        protected List<Long> listLong = new ArrayList<>(20);
 
-        public AnyType(int [] array){
-            List<Integer> list = new ArrayList<>(20);
+
+        public AnyType(int[] array){
             for (int e : array)
-                list.add(e);
+                listInt.add(e);
+            System.out.println(listInt.toString());
+
+
         }
+        public AnyType(long [] array){
+            for (long e : array)
+                listLong.add(e);
+            System.out.println(listLong.toString());
+        }
+
     }
 
     public static void main(String[] args) {
-        int[]arr = {1,2,3,4};
-        AnyType anyType = new AnyType(arr);
-        System.out.println(anyType.toString());
+        int[]arrInt = {1,2,3,4};
+        AnyType anyType1 = new AnyType(arrInt);
+        long[]arrLong = {1,2,4};
+        AnyType anyType2 = new AnyType(arrLong);
+        System.out.println(anyType1.listInt.get(2));
+        System.out.println(anyType2.listLong.get(0));
     }
 }
