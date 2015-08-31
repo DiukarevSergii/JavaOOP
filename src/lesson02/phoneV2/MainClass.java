76629d19d0560f4762e9bc256a6b5730ab3f9774
Diukarev Sergii <vmesteprotiv@gmail.com>
2015-08-31 18:53:56 +0300

    found number of phone

--- src/lesson02/phoneV2/MainClass.java
+++ src/lesson02/phoneV2/MainClass.java
@@ -5,38 +5,49 @@ public class MainClass {
 	public static void main(String[] args) {
 		// Phone p = new Phone(); ошибка т.к. класс абстрактный
 		
-		Nokia3310 nokia = new Nokia3310();
-		System.out.println("Nokia3310 screent size: " + nokia.getScreenSize());
-		nokia.call("123-45-67");
-		nokia.sendSMS("567-78-89", "text message");
-		
-		System.out.println("----------------------------------");
-		
-		IPhone iphone = new IPhone();
-		System.out.println("IPhone screent size: " + iphone.getScreenSize());
-		iphone.call("123-45-67");
-		iphone.sendSMS("567-78-89", "text message");
-		
-		System.out.println("----------------------------------");
-		
-		IPhone5 iphone5 = new IPhone5();
-		System.out.println("IPhone screent size: " + iphone5.getScreenSize());
-		iphone5.call("123-45-67");
-		iphone5.sendSMS("567-78-89", "text message");
-
-
-		System.out.println("----------------------------------");
+//		Nokia3310 nokia = new Nokia3310();
+//		System.out.println("Nokia3310 screent size: " + nokia.getScreenSize());
+//		nokia.call("123-45-67");
+//		nokia.sendSMS("567-78-89", "text message");
+//
+//		System.out.println("----------------------------------");
+//
+//		IPhone iphone = new IPhone();
+//		System.out.println("IPhone screent size: " + iphone.getScreenSize());
+//		iphone.call("123-45-67");
+//		iphone.sendSMS("567-78-89", "text message");
+//
+//		System.out.println("----------------------------------");
+//
+//		IPhone5 iphone5 = new IPhone5();
+//		System.out.println("IPhone screent size: " + iphone5.getScreenSize());
+//		iphone5.call("123-45-67");
+//		iphone5.sendSMS("567-78-89", "text message");
+//
+//
+//		System.out.println("----------------------------------");
 
 		SamsungS4 samsungS4 = new SamsungS4();
 		SamsungS4 samsungS5 = new SamsungS4();
-		System.out.println("SamsungS4 screen size: " + samsungS4.getScreenSize());
-		samsungS4.call("911");
-		samsungS4.sendSMS("911", "message1");
-		samsungS4.sendSMS("911", "message2");
-		samsungS4.sendSMS("911", "message2");
-		samsungS5.sendSMS("911", "message1");
+//		System.out.println("SamsungS4 screen size: " + samsungS4.getScreenSize());
+		samsungS4.setNumber("113", samsungS4);
+		samsungS4.setNumber("114", samsungS4);
+		samsungS4.setNumber("118", samsungS4);
 
-		System.out.println(samsungS4.countSMS);
-		System.out.println(samsungS5.countSMS);
+		System.out.println();
+
+		samsungS4.call("114");
+		samsungS4.call("113");
+		samsungS4.call("911");
+		samsungS4.setNumber("911", samsungS4);
+		samsungS4.call("911");
+
+//		samsungS4.sendSMS("911", "message1");
+//		samsungS4.sendSMS("911", "message2");
+//		samsungS4.sendSMS("911", "message2");
+//		samsungS5.sendSMS("911", "message1");
+
+//		System.out.println(samsungS4.countSMS);
+//		System.out.println(samsungS5.countSMS);
 	}
 }
--- src/lesson02/phoneV2/SamsungS4.java
+++ src/lesson02/phoneV2/SamsungS4.java
@@ -9,7 +9,7 @@ public class SamsungS4 extends Phone {
 
     public int countCall;
     public int countSMS;
-    public static Map<String, String> set = new HashMap<>();
+    public static Map<String, SamsungS4> map = new HashMap<>();
 
     public SamsungS4() {
         System.out.println("SamsungS4 constructor");
@@ -21,10 +21,16 @@ public class SamsungS4 extends Phone {
         hasWifi = true;
     }
 
+    public void setNumber(String n, SamsungS4 samsungS4) {
+        String number = n;
+        map.put(number, samsungS4);
+    }
+
     @Override
     final public void call(String number) {
         countCall++;
-        System.out.println("SamsungS4 class is calling " + number);
+//        System.out.println("SamsungS4 class is calling " + number);
+        findPhone(number);
     }
 
     @Override
@@ -33,4 +39,24 @@ public class SamsungS4 extends Phone {
         System.out.println("SamsungS4 class is sending sms " + message + " to " + number + " Hello");
     }
 
+    public void findPhone(String number){
+        boolean found = false;
+
+        for (Map.Entry<String,SamsungS4> pair : map.entrySet()){
+            if (pair.getKey().equals(number)) {
+                pair.getValue().answer(number);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+            System.out.println(number + ": Not found!");
+
+        System.out.println();
+    }
+
+    public void answer(String number){
+        System.out.println(number + ": Answer");
+    }
 }
