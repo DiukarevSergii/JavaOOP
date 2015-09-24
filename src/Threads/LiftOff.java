9740bbf238cdd7cfb890d590a59916d0ce4b9ed9
Diukarev Sergii <vmesteprotiv@gmail.com>
2015-09-24 16:41:42 +0300

    java.util.concurrent.*

---
+++ src/Threads/LiftOff.java
@@ -0,0 +1,34 @@
+package Threads;
+
+import java.util.concurrent.TimeUnit;
+
+public class LiftOff implements Runnable {
+    protected int countDown = 10; // Значение по умолчанию
+    private static int taskCount = 0;
+    private final int id = taskCount++;
+
+    public LiftOff() {
+    }
+
+    public LiftOff(int countDown) {
+        this.countDown = countDown;
+    }
+
+    public String status() {
+        return "#" + id + "(" +
+                (countDown > 0 ? countDown : "Liftoff!") + "), ";
+    }
+
+    public void run() {
+        while (countDown-- > 0) {
+            System.out.print(status());
+//            Thread.yield();
+            try {
+                TimeUnit.MILLISECONDS.sleep(100); //новый стиль
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println();
+    }
+}
---
+++ src/Threads/MainThread.java
@@ -0,0 +1,9 @@
+package Threads;
+
+public class MainThread {
+    public static void main(String[] args) {
+
+        LiftOff launch = new LiftOff();
+        launch.run();
+    }
+}
